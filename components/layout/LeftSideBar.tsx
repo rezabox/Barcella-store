18f40c11711b90a42a9082888735b93cb0b3389f
rezabox <reza.asareh81@gmail.com>
2024-10-29 22:50:17 +0330

    add pathName to the Link

--- components/layout/LeftSideBar.tsx
+++ components/layout/LeftSideBar.tsx
@@ -1,17 +1,19 @@
+'use client';
 import React from 'react'
 import Image from 'next/image'
 import { navLinks } from '@/lib/constants';
 import Link from 'next/link';
 import { UserButton } from '@clerk/nextjs';
+import { usePathname } from 'next/navigation';
 
 function LeftSideBar() {
-  
+  const pathName = usePathname();
   return (
     <div className='h-screen left-0 top-0 sticky p-10 flex flex-col gap-16 bg-blue-2 shadow-xl max-lg:hidden'>
       <Image src="/logo.png"  width={150} height={70} alt='logo'/>
       <div className='flex flex-col gap-12'>
          {navLinks.map((link)=>(
-             <Link href={link.url} key={link.label} className='flex gap-4 text-body-medium'>
+             <Link href={link.url} key={link.label} className={`flex gap-4 text-body-medium ${pathName === link.url ? 'text-blue-1' : ''}`}>
               <span>{link.icon}</span>          
               {link.label}
           </Link>
@@ -24,5 +26,4 @@ function LeftSideBar() {
     </div>
   )
 }
-
 export default LeftSideBar;
